6f11dc3c1459bbe0bed514030d25950833561e72
YaredAlex <yareda25@gmail.com>
2024-08-12 00:26:07 +0530

    appliedAgentList

--- src/features/assign/hook/use_assign_project.tsx
+++ src/features/assign/hook/use_assign_project.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {
   ClientProjectType,
+  PostedProjectType,
   useProjectContext,
 } from "../../../context/projects/project_context";
 import { useNavigate, useParams } from "react-router-dom";
@@ -8,12 +9,23 @@ import { useAxios } from "../../../hooks/useAxios";
 import customToast from "../../../components/custom_toast/custom_toast";
 import { useGetProjectById } from "../../../hooks/use_get_project_id";
 import { AxiosResponse } from "axios";
+import { AgentDetailType } from "../../../context/agent/agent_context";
 
+export type AppliedAgentType = {
+  proposal: string;
+  applied_at: string;
+  status: string;
+  frelancer_id: AgentDetailType;
+  id: number;
+  project_id: number;
+};
 const useAssignProject = () => {
   // const [project, setProject] = useState(1);
   const { id } = useParams();
   const { currentProject, setCurrentProject } = useProjectContext();
   const [fetchProject, setFetchProject] = useState(false);
+  const [agentList, setAgentList] = useState<AppliedAgentType[]>([]);
+  const [showModal, setShowModal] = useState(false);
   const getProject = useGetProjectById();
   const { sendRequest, loading } = useAxios({
     url: "/api/user/apply_project/",
@@ -25,15 +37,17 @@ const useAssignProject = () => {
   // const controller = new AbortController();
 
   useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
     if (id != null || id != undefined) {
       getProject.getProject(id, (res) => {
         setCurrentProject(res.data.serialized_data);
         console.log(res.data);
       });
       //call get freelancers
-      // getAppliedFreelancers.getFreelancers( (res)=>{
-      //   console.log(res);
-      // })
+      getAppliedFreelancers.getFreelancers((res) => {
+        console.log(res);
+        setAgentList(res.data.serialized_data);
+      });
     } else navigate("/admin/dashboard/");
     return () => {};
   }, [fetchProject]);
@@ -63,22 +77,28 @@ const useAssignProject = () => {
     currentProject,
     assignProject,
     setFetchProject,
+    showModal,
+    setShowModal,
+    agentList,
     loadingFreelancers: getAppliedFreelancers.loading,
   };
 };
 export type useAssignProjectType = {
   getProjectLoading: boolean;
   loading: boolean;
-  currentProject: ClientProjectType;
+  currentProject: ClientProjectType | PostedProjectType;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+  showModal: boolean;
   assignProject: () => void;
 };
 export default useAssignProject;
 
 const useGetAppliedFreelancers = () => {
+  const { id } = useParams();
   const { loading, sendRequest } = useAxios({
     headers: true,
     method: "GET",
-    url: "",
+    url: `api/user/applied/freelancers/${id}/`,
   });
 
   const getFreelancers = (cb: (res: AxiosResponse) => void) => {
