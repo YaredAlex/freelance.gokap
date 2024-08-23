d4dae10e70499ba72cde6db6061422d0d75df45e
YaredAlex <yareda25@gmail.com>
2024-08-23 15:17:37 +0530

    adding privacty link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,10 @@
 import "./footer.css";
 import GITLogo from "../logo/logo";
 import { Link } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+// import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
 const Footer = () => {
-  const iconSize = 24;
-  const iconColor = "white";
+  // const iconSize = 24;
+  // const iconColor = "white";
   return (
     <div className="section-footer pb-4 pt-4">
       <div className="max-w-1200 mx-auto  d-flex flex-column px-2">
@@ -12,12 +12,14 @@ const Footer = () => {
         <div className="d-flex justify-content-between align-items-center py-2 mb-2">
           <GITLogo />
           <div className="d-flex gap-3">
-            <Link to={""}>About GIT</Link>
-            <Link to={""}>Privacy</Link>
+            <Link to={"https://gokapinnotech.com/"} target="_blank">
+              About GIT
+            </Link>
+            <Link to={"/privacy"}>Privacy Policy</Link>
           </div>
         </div>
         {/*  */}
-        <div className="footer-social  d-flex justify-content-end gap-3">
+        {/* <div className="footer-social  d-flex justify-content-end gap-3">
           <Link to={""}>
             <FaFacebook size={iconSize} color={iconColor} />
           </Link>
@@ -27,7 +29,7 @@ const Footer = () => {
           <Link to={""}>
             <FaLinkedin size={iconSize} color={iconColor} />
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
