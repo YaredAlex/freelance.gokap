3d684e1a37ebb5bf853de89912d4cd8f7486b335
YaredAlex <yareda25@gmail.com>
2024-09-06 22:57:24 +0530

    changing base_url

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,5 +1,5 @@
-// export const base_url = "https://gokap.onrender.com";
-export const base_url = "http://localhost:8000"
+export const base_url = "https://gokap.onrender.com";
+// export const base_url = "http://localhost:8000"
 export const signInApiPoint = "/api/user/login/";
 export const signUpApiPoint = "/api/user/signup";
 export const profileApi = "/api/user/profile/";
