bf8c498e75aeb2bb86214663968dd34cf44589f2
Rupesh <21bcs11201@gmail.com>
2025-05-05 17:07:01 +0530

    fix darktheme

--- src/components/custom_toast/toast_container.tsx
+++ src/components/custom_toast/toast_container.tsx
@@ -1,10 +1,15 @@
 import { ToastContainer } from "react-toastify";
 import { useThemeContext } from "../../context/theme/theme_context";
 const CustomToastContainer = () => {
-  const { isDark } = useThemeContext();
-  if (isDark) return <ToastContainer theme="dark" />;
+	const { isDark } = useThemeContext();
+	if (isDark) return <ToastContainer theme="dark" />;
 
-  return <ToastContainer theme="light" />;
+	return (
+		<ToastContainer
+			theme="light"
+			style={{ minHeight: "auto" }}
+		/>
+	);
 };
 
 export default CustomToastContainer;
