3f9bdccaff540e85faec2c778e4500adaca72cf6
YaredAlex <yareda25@gmail.com>
2025-05-05 12:13:43 +0530

    created use_change_password

--- src/features/profile/components/change_password.tsx
+++ src/features/profile/components/change_password.tsx
@@ -1,21 +1,19 @@
 import DefaultModal from "../../../components/popup/modal";
-import {
-  UseClientProfileType,
-  useChangeClientPassword,
-} from "../hooks/client/use_client_profile";
+import { UseClientProfileType } from "../hooks/client/use_client_profile";
 import {
   ButtonPrimary,
   ButtonPrimaryOutline,
 } from "../../../components/button/button";
 import TextField from "../../../components/inputField/text_field";
 import { AgentProfileProp } from "../hooks/agent/use_agent_profile";
+import { useChangePassword } from "../hooks/usechangepassword";
 
 const ChangePassword = ({
   clientProfile,
 }: {
   clientProfile: UseClientProfileType | AgentProfileProp;
 }) => {
-  const changePassword = useChangeClientPassword();
+  const changePassword = useChangePassword();
   return (
     <DefaultModal
       loading={changePassword.loading}
---
+++ src/features/profile/hooks/usechangepassword.tsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+import { useAxios } from "../../../hooks/useAxios";
+import customToast from "../../../components/custom_toast/custom_toast";
+
+export const useChangePassword = () => {
+  const { loading, sendRequest } = useAxios({
+    url: "/api/user/change_password/",
+    method: "POST",
+    headers: true,
+  });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      password: "",
+      cnfpassword: "",
+      currentPassword: "",
+    },
+  });
+
+  //Change password
+  const changePassword = (data: { password: string; cnfpassword: string }) => {
+    sendRequest(
+      data,
+      () => {
+        customToast({ message: "Password Changed", type: "success" });
+        reset();
+      },
+      (error) => {
+        console.log(error);
+        customToast({ message: error.message, type: "error" });
+      }
+    );
+  };
+
+  return {
+    loading,
+    changePassword,
+    register,
+    handleSubmit,
+    errors,
+    reset,
+  };
+};
